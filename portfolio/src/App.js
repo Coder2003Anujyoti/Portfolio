aefb7f1a35e735ba4902abea2fd38892f3af37b7
Anujyoti De <anujyotide@gmail.com>
2025-01-21 22:35:00 +0530

    Update App.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,6 @@
 import {HashRouter as Routers,
 Routes,Route,Link} from 'react-router-dom';
-import React,{useState} from 'react';
-import './App.css';
+import React,{useState,useEffect} from 'react';
 import Home from './Project/Home.js';
 import About from './Project/About.js';
 import Skills from './Project/Skills.js';
@@ -12,19 +11,32 @@ function App() {
   const my={
     display:`${toggle?"flex":"none"}`
   }
+  useEffect(() => {
+    document.body.className ="bg-[#081c29]"; 
+  }, []);
   return (
     <>
- <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
       <Routers>
-          <div id="nav-bar">
-            <h1 id="nav-heading">My Portfolio</h1>
-       {!toggle && <> <img src="images/bar.png" onClick={()=>setToggle(!toggle)} id="toggle" /> 
+          <div className="w-full flex items-center p-4 bg-[#081c29]">
+          <div className="text-xl w-48  font-extrabold"> <h1 className="bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">My Portfolio</h1></div>
+    <div className="md:hidden w-full flex justify-end">   {!toggle && <> <img className="w-8 h-8"src="images/bar.png" onClick={()=>setToggle(!toggle)} id="toggle" /> 
        </>}
-       {toggle && <>  <img src="images/closes.png"  id="close" onClick={()=>setToggle(!toggle)} /></>}
-          </div>
-         <div id="show-details" style={my}>
-        <ul type="none">
+       {toggle && <>  <img src="images/closes.png" className="w-4 h-4" id="close" onClick={()=>setToggle(!toggle)} /></>}
+       </div>
+      <div className="hidden md:w-full  md:flex md:flex-row">
+         <ul type="none" className="hidden md:w-full  md:flex md:flex-row md:justify-evenly md:text-white md:font-extrabold">
         <li id="home"><Link id="link" 
+         to="/">Home</Link></li>
+      <li><Link id="link" to="/about">About</Link></li>
+       <li><Link id="link" to="/skills">Skills</Link></li>
+         <li><Link id="link" to="/projects">Projects</Link></li>
+       <li><Link id="link" to="/contacts">Contacts</Link></li>
+        </ul>
+      </div>
+          </div>
+         <div className="p-4 flex justify-center gap-y-8 font-extrabold text-white bg-[#081c29] "style={my}>
+        <ul type="none" className="p-2 flex justify-center gap-y-8 font-extrabold text-white flex-col">
+     <li id="home"><Link id="link" 
          to="/" onClick={()=>setToggle(false)}>Home</Link></li>
       <li><Link id="link" to="/about" onClick={()=>setToggle(false)}>About</Link></li>
        <li><Link id="link" to="/skills" onClick={()=>setToggle(false)}>Skills</Link></li>
