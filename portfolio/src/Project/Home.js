6b8eedb8af3c99a634730ff9fb024112ca340d7f
Anujyoti De <anujyotide@gmail.com>
2025-01-21 22:37:35 +0530

    Update Home.js

--- portfolio/src/Project/Home.js
+++ portfolio/src/Project/Home.js
@@ -1,18 +1,20 @@
 import React,{useEffect,useState} from "react";
-import './Home.css'
 const Home = () => {
   return(
     <>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
-    <div id="home-container">
-      <div  id="image-container">
-        <img src="images/AnujyotiDe.jpg" />
-      </div>
-    <h2 id="title">I'm a <span className="text">Frontend Developer</span></h2>
-        <p id="infos">Hi, I’m Anujyoti De, a passionate B.Tech student specializing in Information Technology. Currently, I am honing my skills in the field of Frontend Development, with a keen interest in creating dynamic and user-friendly web applications.
+    <div className="p-4  w-full flex items-center flex-col md:flex-row  md:justify-start">
+        <img className="w-64 h-64 rounded-full" src="images/AnujyotiDe.jpg" />
+              <div className="p-8">
+      <div className="w-full  text-xs text-white font-extrabold">
+    <h2 className="text-white font-extrabold">I'm a <span className=" text-xl bg-gradient-to-r from-purple-500 to-pink-500 inline-block text-transparent bg-clip-text">Web Developer</span></h2>
+        <p className="text-white font-extrabold" id="infos">Hi, I’m Anujyoti De, a passionate B.Tech student specializing in Information Technology. Currently, I am honing my skills in the field of Frontend Development, with a keen interest in creating dynamic and user-friendly web applications.
         </p>
-            <a href="https://www.linkedin.com/in/anujyoti-de-1a365a258" target="blank_"><img src="images/linkedin.png" id="linkedin" /></a>
-      <a href="https://github.com/Coder2003Anujyoti" target="blank_"><img src="images/github.png" id="github"/></a>
+    </div>
+      <div className="w-full flex flex-row justify-start my-4 gap-x-4">
+            <a href="https://www.linkedin.com/in/anujyoti-de-1a365a258" target="blank_"><img className="w-8 h-8" src="images/linkedin.png" id="linkedin" /></a>
+      <a href="https://github.com/Coder2003Anujyoti" target="blank_"><img src="images/github.png" className="w-8 h-8" id="github"/></a>
+      </div>
+      </div>
     </div>
     </>
   );
