a2cfb223ea9450978d915ac79212b85da6cf2a1e
Anujyoti De <anujyotide@gmail.com>
2025-01-21 22:38:41 +0530

    Update Projects.js

--- portfolio/src/Project/Projects.js
+++ portfolio/src/Project/Projects.js
@@ -1,44 +1,23 @@
 import React from "react";
-import './Projects.css'
 const Projects = () => {
   return (
+    <>
 <div id="projects-container">
-  <h1 id="project-heading">My Projects</h1>
-      <div id="project-images">
-   <div id="project-1">
- <a href="https://coder2003anujyoti.github.io/My-Gaming-Website/" target="_blank"><img src="Images/Gaming.jpg"  /></a>
-  <h1>Gaming Website</h1>
-  <p>Welcome to  My Gaming Website, the ultimate place for fun and excitement! Here, you can play a variety of interactive games  from the comfort of your device. </p>
+  <div className="w-full flex justify-center "><h1 className="text-xl font-extrabold bg-gradient-to-r from-purple-500 to-pink-500 inline-block text-transparent bg-clip-text ">My Projects</h1></div>
+      <div className="py-4 flex flex-col gap-y-8 md:flex-row">
+       <div className="flex flex-col text-center gap-y-2">
+ <div className="w-full flex justify-center"><a href="https://coder2003anujyoti.github.io/Pokedex/" target="_blank"><img src="Images/Pokemon.jpg" className="w-64 h-54 rounded-lg"/></a></div>
+  <h1 className="text-xl text-white font-extrabold">Pokedex App</h1>
+    <p className="text-sm text-white font-extrabold">Welcome to the Pokedex App! It is an API inbuilt app.Here you get different variety of pokemons along with their stats. Let's enjoy it by explore it.</p>
   </div>
-     <div id="project-1">
-  <a href="https://coder2003anujyoti.github.io/Voting-Website/" target="_blank"><img src="Images/Voting.jpg"  /></a>
-  <h1>Voting Website</h1>
-  <p>Welcome to the Voting Website! This platform is designed to help users learn about the Electronic Voting Machine (EVM) and its process.</p>
+         <div className="flex flex-col text-center gap-y-2">
+ <div className="w-full flex justify-center"><a href="https://coder2003anujyoti.github.io/Ben10-website/" target="_blank"><img src="Images/Ben10.jpg" className="w-64 h-54 rounded-lg"/></a></div>
+  <h1 className="text-xl text-white font-extrabold">Ben10 App</h1>
+    <p className="text-sm text-white font-extrabold">Welcome to the Ben10 App! It is a REST API inbuilt app .Here you get different variety of aliens along with their names and images. Creating REST API for storing data as well as introduce different methods for fetching data in REST API Let's enjoy it by explore it.</p>
   </div>
-       <div id="project-1">
- <a href="https://coder2003anujyoti.github.io/Ipl-App/#profilepage" target="_blank"><img src="Images/Ipl.jpg"  /></a>
-  <h1>Ipl Website</h1>
-  <p>Welcome to the IPL App!  The ultimate app for every cricket fan! With the IPL App, you can also view detailed player stats and team line-ups.</p>
-  </div>
-       <div id="project-1">
- <a href="https://coder2003anujyoti.github.io/Hotel/" target="blank"><img src="Images/Menu.jpg"  /></a>
-  <h1>Restaurant Website</h1>
-  <p>Welcome to the Restaurant Website! Here you get different variety of foods at different period of time. Let's enjoy it by explore it.</p>
-  </div>
-       <div id="project-1">
- <a href="https://coder2003anujyoti.github.io/React/" target="_blank"><img src="Images/Kabaddi.png"  /></a>
-  <h1>Kabaddi Game App</h1>
-    <p>Welcome to the Kabaddi Game App! Here you test your analytical and critical thinking skills through a simple game.Let's enjoy it by explore it. </p>
-  </div>
-       <div id="project-1">
-  <a href="https://coder2003anujyoti.github.io/Pokedex/" target="_blank"><img src="Images/Pokemon.jpg"  /></a>
-  <h1>Pokedex App</h1>
-    <p>Welcome to the Pokedex App! It is an API inbuilt app.Here you get different variety of pokemons along with their stats. Let's enjoy it by explore it.</p>
-  </div>
-  
-
   </div>
 </div>
+</>
   );
 };
 
