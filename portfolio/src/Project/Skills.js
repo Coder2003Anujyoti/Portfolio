ce3149ee939a83c89fb5e8c92ef292cf449af2c3
Anujyoti De <anujyotide@gmail.com>
2025-01-22 22:41:00 +0530

    Update Skills.js

--- portfolio/src/Project/Skills.js
+++ portfolio/src/Project/Skills.js
@@ -2,17 +2,18 @@ import React from "react";
 const Skills = () => {
   return (
     <>
+    <div className="md:p-4">
     <div className="w-full flex justify-center">
     <h1 className="text-xl bg-gradient-to-r from-purple-500 to-pink-500 inline-block text-transparent bg-clip-text font-extrabold">My Skills</h1>
     </div>
-    <div className="p-8 flex  flex-col gap-8 md:flex-col md:items-center">
+    <div className="p-8 flex  flex-col gap-8 md:flex-col md:items-center md:text-xl md:p-8">
 <div className="flex gap-2 flex-row text-white font-extrabold text-md">
   <img src="images/html-5.png" className="w-8 h-8" />
-   <div className="w-96 flex bg-black rounded-full text-white"><div className="w-48 bg-gradient-to-r from-purple-500 to-pink-500 rounded-full md:w-80 "></div></div>80%
+   <div className="w-96 flex bg-black rounded-full text-white md:text-lg"><div className="w-48 bg-gradient-to-r from-purple-500 to-pink-500 rounded-full md:w-80 "></div></div>80%
 </div>
 <div className="flex gap-2 flex-row text-white font-extrabold text-md">
   <img src="images/css-3.png" className="w-8 h-8" />
-   <div className="w-96 flex bg-black rounded-full text-white"><div className="w-48 bg-gradient-to-r from-purple-500 to-pink-500 rounded-full md:w-80"></div></div>80%
+   <div className="w-96 flex bg-black rounded-full text-white md:text-lg"><div className="w-48 bg-gradient-to-r from-purple-500 to-pink-500 rounded-full md:w-80"></div></div>80%
 </div>
 <div className="flex gap-2 flex-row text-white font-extrabold text-md">
   <img src="images/js.png" className="w-8 h-8" />
@@ -38,8 +39,20 @@ const Skills = () => {
   <img src="images/C.png" className="w-8 h-8" />
    <div className="w-96 flex bg-black rounded-full text-white"><div className="w-48 bg-gradient-to-r from-purple-500 to-pink-500 rounded-full md:w-80"></div></div>80%
 </div>
+<div className="flex gap-2 flex-row text-white font-extrabold text-md">
+  <img src="images/python.png" className="w-8 h-8" />
+   <div className="w-96 flex bg-black rounded-full text-white"><div className="w-16 bg-gradient-to-r from-purple-500 to-pink-500 rounded-full md:w-20"></div></div>20%
+</div>
+<div className="flex gap-2 flex-row text-white font-extrabold text-md">
+  <img src="images/Tailwind CSS.png" className="w-8 h-8" />
+   <div className="w-96 flex bg-black rounded-full text-white"><div className="w-36 bg-gradient-to-r from-purple-500 to-pink-500 rounded-full md:w-60"></div></div>60%
+</div>
+<div className="flex gap-2 flex-row text-white font-extrabold text-md">
+  <img src="images/SQL.png" className="w-8 h-8" />
+   <div className="w-96 flex bg-black rounded-full text-white"><div className="w-16 bg-gradient-to-r from-purple-500 to-pink-500 rounded-full md:w-20"></div></div>20%
+</div>
   </div>
-
+</div>
     </>
   );
 };
