8291806af07398bf169464afc8be8037017cfc6a
Anujyoti De <anujyotide@gmail.com>
2025-01-22 22:39:48 +0530

    Update Home.js

--- portfolio/src/Project/Home.js
+++ portfolio/src/Project/Home.js
@@ -2,12 +2,12 @@ import React,{useEffect,useState} from "react";
 const Home = () => {
   return(
     <>
-    <div className="p-4  w-full flex items-center flex-col lg:flex-row  lg:justify-start lg:p-16">
+    <div className="p-4  w-full flex items-center flex-col lg:flex-row lg:justify-start lg:p-16">
         <img className="w-64 h-64 md:w-96 md:h-96 rounded-full" src="images/AnujyotiDe.jpg" />
-              <div className="p-8">
-      <div className="w-full  text-xs text-white font-extrabold">
-    <h2 className="text-white lg:text-lg">I'm a <span className=" text-xl bg-gradient-to-r from-purple-500 to-pink-500 inline-block text-transparent bg-clip-text font-extrabold md:text-2xl">Web Developer</span></h2>
-        <p className="text-white font-extrabold md:text-lg" id="infos">Hi, I’m Anujyoti De, a passionate B.Tech student specializing in Information Technology. Currently, I am honing my skills in the field of Frontend Development, with a keen interest in creating dynamic and user-friendly web applications.
+              <div className="p-4">
+      <div className="w-full  text-xs text-white">
+    <h2 className="text-white text-sm font-bold lg:text-lg">I'm a <span className=" text-xl bg-gradient-to-r from-purple-500 to-pink-500 inline-block text-transparent bg-clip-text font-extrabold md:text-2xl">Web Developer</span></h2>
+        <p className="text-white  text-sm font-bold lg:text-lg" id="infos">Hi, I’m Anujyoti De, a passionate B.Tech student specializing in Information Technology. Currently, I am honing my skills in the field of Frontend Development, with a keen interest in creating dynamic and user-friendly web applications.
         </p>
     </div>
       <div className="w-full flex flex-row justify-start my-4 gap-x-4">
