171d5af44ffb81697565cec1fa5e326295ca4f18
Anujyoti De <anujyotide@gmail.com>
2025-01-30 21:05:36 +0530

    Update Projects.js

--- portfolio/src/Project/Projects.js
+++ portfolio/src/Project/Projects.js
@@ -15,6 +15,11 @@ const Projects = () => {
   <h1 className="text-xl text-white font-bold">Ben10 App</h1>
     <p className="text-sm text-white font-bold lg:text-lg">Welcome to the Ben10 App! It is a REST API inbuilt app .Here you get different variety of aliens along with their names and images. Creating REST API for storing data as well as introduce different methods for fetching data in REST API Let's enjoy it by explore it.</p>
   </div>
+     <div className="flex flex-col text-center gap-y-2">
+ <div className="w-full flex justify-center"><a href="https://coder2003anujyoti.github.io/benweb/" target="_blank"><img src="images/IP.jpg" className="w-64 h-54 rounded-lg"/></a></div>
+  <h1 className="text-xl text-white font-bold">Cricket App</h1>
+    <p className="text-sm text-white font-bold lg:text-lg">Welcome to the Cricket App! It is a mern-stack based app involve both frontend showing images and different details and backend for storing data and fetch data through different HTTP requests.</p>
+  </div>
   </div>
 </div>
 </>
